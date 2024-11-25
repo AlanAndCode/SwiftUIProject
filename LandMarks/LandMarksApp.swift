faf53210103974d8a94d4c5f093d1c34535cf49b
AlanAndCode <alan.andersonbr@gmail.com>
2024-11-25 11:49:33 -0300

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by Alan Modesto on 25/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LandMarksTests/LandMarksTests.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksTests.swift
+//  LandMarksTests
+//
+//  Created by Alan Modesto on 25/11/24.
+//
+
+import Testing
+@testable import LandMarks
+
+struct LandMarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
