5ad773e7f3deb0aea6abfd5a494a04afaaa4840d
AlanAndCode <alan.andersonbr@gmail.com>
2024-12-06 15:07:36 -0300

    feat: add badges forms to hexagon

---
+++ LandMarks/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,23 @@
+//
+//  RotatedBadgeSymbol.swift
+//  LandMarks
+//
+//  Created by Alan Modesto on 03/12/24.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
